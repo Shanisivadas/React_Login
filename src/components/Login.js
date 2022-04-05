ce08edb54b263182799b1e27efffc894643ab544
anisha.p <anisha.p@chillarcards.com>
2022-04-05 17:17:56 +0530

    Final Changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,6 +6,7 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
 import { useNavigate } from "react-router-dom"
 import Home from "./Home";
 function Login() {
@@ -29,34 +30,50 @@ function Login() {
         }
     }
     return (
-        <div>
-            <h2>WELCOME</h2>
-            <Card sx={{ minWidth: 275 }}>
-                <CardContent>
-                    <div>
-                        <TextField
-                            required
-                            id="outlined-required"
-                            label="UserName"
-                            placeholder="Enter the Username"
-                            onChange={(e) => setUserName(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <TextField
-                            required
-                            id="outlined-password-input"
-                            label="Password"
-                            type="password"
-                            placeholder="Enter the Password"
-                            onChange={(e) => setPassword(e.target.value)} />
-                    </div>
-                    <div
-                    ><Button variant="outlined" onClick={checkLogin}>Login</Button>
-                    </div>
-                </CardContent>
-                {error ? <h4>Username or Password is incorrect</h4> : ""}
-            </Card>
+        <div><Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            style={{ minHeight: '50vh' }}
+        >
+
+            <Grid item xs={3}>
+
+
+                <h2>WELCOME TO LOGIN</h2>
+
+                <Card sx={{ minWidth: 275 }}>
+                    <CardContent>
+                        <div >
+                            <TextField
+                                required
+                                id="outlined-required"
+                                label="UserName"
+                                placeholder="Enter the Username"
+                                onChange={(e) => setUserName(e.target.value)}
+                            />
+                        </div>
+                        <div>
+                            <TextField
+                                required
+                                id="outlined-password-input"
+                                label="Password"
+                                type="password"
+                                placeholder="Enter the Password"
+                                onChange={(e) => setPassword(e.target.value)} />
+                        </div>
+                        <div><Box textAlign='center'>
+                            <Button variant="outlined" align-items="center" onClick={checkLogin}>Login</Button>
+                        </Box>
+
+                        </div>
+                    </CardContent>
+                    {error ? <h4>Username or Password is incorrect</h4> : ""}
+                </Card>  </Grid>
+
+        </Grid>
         </div>
 
     )
